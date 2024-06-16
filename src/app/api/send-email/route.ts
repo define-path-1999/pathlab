acabc3cf8046fec6f7c2dddb67794651fdbec65c
Rishi Singh <rs3949427@gmail.com>
2024-06-16 19:01:24 +0530

    Fix Email UI

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -48,47 +48,38 @@ export async function POST(req: NextRequest) {
       // to: "guptaayush617@gmail.com",
       to: process.env.EMAIL,
       subject: `A Booking created`,
-      html: `
-        <div style="background: linear-gradient(to bottom right, #e0f7fa, #b2ebf2); padding: 20px; max-width: 600px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #b2dfdb; margin: 20px auto; background-color: #ffffff; padding-bottom: 40px;">
-          <div style="display:flex;flex-direction:row-reverse; justify-content:space-between; align-items:center;border-bottom: 2px solid #00796b; padding-bottom: 10px; margin-bottom: 20px;">
-            <img src="https://via.placeholder.com/100" alt="Logo" style="width: 50px; height: 50px;">
-            <h2 style="font-family: Georgia, serif; color: #00796b; ">Booking Confirmed!</h2>
-          </div>
-          <div>
-              <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
-                  <i class="fas fa-user" style="color: #00796b; margin-right: 5px;"></i>
-                  <strong style="font-weight: bold;">Name:</strong> <span>${name}</span>
-              </p>
-              <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
-                  <i class="fas fa-phone" style="color: #00796b; margin-right: 5px;"></i>
-                  <strong style="font-weight: bold;">Contact Number:</strong> <span>${contactNumber}</span>
-              </p>
-              <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
-                  <i class="fas fa-clock" style="color: #00796b; margin-right: 5px;"></i>
-                  <strong style="font-weight: bold;">Booking Time:</strong> <span>${formatedDate}</span>
-              </p>
-          </div>
-          <h3 style="font-family: Georgia, serif; color: #00897b; border-bottom: 2px solid #00897b; padding-bottom: 10px; margin-bottom: 20px;">Your Booking Details</h3>
-          <div>
-              <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
-                  <i class="fas fa-map-marker-alt" style="color: #00897b; margin-right: 5px;"></i>
-                  <strong style="font-weight: bold;">Flat Number:</strong> <span>${flatNumber}</span>
-              </p>
-              <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
-                  <i class="fas fa-building" style="color: #00897b; margin-right: 5px;"></i>
-                  <strong style="font-weight: bold;">Building Number:</strong> <span>${buildingNumber}</span>
-              </p>
-              <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
-                  <i class="fas fa-landmark" style="color: #00897b; margin-right: 5px;"></i>
-                  <strong style="font-weight: bold;">Landmark:</strong> <span>${landmark}</span>
-              </p>
-              <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
-                  <i class="fas fa-city" style="color: #00897b; margin-right: 5px;"></i>
-                  <strong style="font-weight: bold;">City:</strong> <span>${city}</span>
-              </p>
-          </div>
-        </div>
-      `,
+      html: `<div style="background: white; padding: 20px; max-width: 600px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #b2dfdb; margin: 20px auto; padding-bottom: 40px;">
+  <div style="display: flex; justify-content: center; align-items: center; border-bottom: 2px solid #3f51b5; padding-bottom: 10px; margin-bottom: 20px;">
+    <h2 style="font-family: Georgia, serif; color: #3f51b5;">Booking Confirmed!</h2>
+  </div>
+  <div>
+    <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
+      <strong style="font-weight: bold;">Name:</strong> <span>${name}</span>
+    </p>
+    <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
+      <strong style="font-weight: bold;">Contact Number:</strong> <span>${contactNumber}</span>
+    </p>
+    <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
+      <strong style="font-weight: bold;">Booking Time:</strong> <span>${formatedDate}</span>
+    </p>
+  </div>
+  <h3 style="font-family: Georgia, serif; color: #3f51b5; border-bottom: 2px solid #3f51b5; padding-bottom: 10px; margin-bottom: 20px;">Your Booking Details</h3>
+  <div>
+    <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
+      <strong style="font-weight: bold;">Flat Number:</strong> <span>${flatNumber}</span>
+    </p>
+    <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
+      <strong style="font-weight: bold;">Building Number:</strong> <span>${buildingNumber}</span>
+    </p>
+    <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
+      <strong style="font-weight: bold;">Landmark:</strong> <span>${landmark}</span>
+    </p>
+    <p style="margin: 10px 0; color: #004d40; font-size: 1.1em;">
+      <strong style="font-weight: bold;">City:</strong> <span>${city}</span>
+    </p>
+  </div>
+</div>
+`,
     };
 
     await transporter.sendMail(mailOptions);
